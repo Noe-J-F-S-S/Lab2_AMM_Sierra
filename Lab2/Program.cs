1a39f92e834d460ba99d2734fa1906d43bd9b279
Noe <noe@gmail.com>
2022-08-31 18:22:30 -0500

    primer commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Lab2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rectangulo = new Rectangulo();
+            
+
+            Vertice v1 = new Vertice { x = 1, y = 2 };
+            Vertice v2 = new Vertice { x = 1, y = 4 };
+            Vertice v3 = new Vertice { x = 5, y = 4 };
+            Vertice v4 = new Vertice { x = 5, y = 2 };
+
+            rectangulo.v1 = v1;
+            rectangulo.v2 = v2;
+            rectangulo.v3 = v3;
+            rectangulo.v4 = v4;
+            Console.WriteLine("//////////////////////////////////////");
+            Console.WriteLine("El Área es: "+rectangulo.GetArea());
+            Console.WriteLine("El Perímetro es: "+rectangulo.GetPerimetro());
+            Console.WriteLine("//////////////////////////////////////");
+
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ Lab2/Rectangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Rectangulo
+    {
+        public Vertice v1 { get; set; }
+        public Vertice v2 { get; set; }
+        public Vertice v3 { get; set; }
+        public Vertice v4 { get; set; }
+        //public int x1 { get; set; }
+        //public int y1 { get; set; }
+        //public int x2 { get; set; }
+        //public int y2 { get; set; }
+        //public int x3 { get; set; }
+        //public int y3 { get; set; }
+        //public int x4 { get; set; }
+        //public int y4 { get; set; }
+
+        public double GetArea()
+        {
+            double base1 = Math.Sqrt(Math.Pow(v4.x - v1.x,2) + (Math.Pow(v4.y - v1.y,2)));
+            double altura = Math.Sqrt(Math.Pow(v2.x - v1.x, 2) + (Math.Pow(v2.y - v1.y, 2)));
+            return base1 * altura;
+        }
+        public double GetPerimetro()
+        {
+            double base1 = Math.Sqrt(Math.Pow(v4.x - v1.x, 2) + (Math.Pow(v4.y - v1.y, 2)));
+            double altura = Math.Sqrt(Math.Pow(v2.x - v1.x, 2) + (Math.Pow(v2.y - v1.y, 2)));
+            return (base1 + altura)*2;
+        }
+    }
+
+    
+}
